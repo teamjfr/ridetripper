be3b089e54b9060b65238edbed93cc49e5c7f63b
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-10-16 13:50:44 +0600

    Contact Page and Images

---
+++ contact.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact </title>
+    <link rel="stylesheet" href="style/contact.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  </head>
+  <body>
+    <header>
+      <div class="inner-width">
+          <span class="logo">Ridetripper</span>
+        <i class="menu-toggle-btn fas fa-bars"></i>
+        <div class="navigation_bar_class">
+          <a href="./homepage.php"><i class="fas fa-home home"></i> Home</a>
+          <a href="./user_profile.php"><i class="fas fa-user user"></i> Profile</a>
+          <a href=""><i class="fas fa-headset contact"></i> Contact</a>
+        </div>
+          
+      </div>
+    </header>
+    <div class="profile">
+    <div class="container">
+      <div class="box">
+      <div class="image">
+          <img src="image/Rafat.jpg">
+        </div>
+        <div class="name_job">Sadat Arefin</div>
+        <div class="name_job"> Rafat</div>
+        <p>180041106</p>
+      </div>
+
+      <div class="box">
+        <div class="image">
+          <img src="image/Jim.jpg" alt="">
+        </div>
+        <div class="name_job">Shams Tanveer</div>
+        <div class="name_job">Jim</div>
+        <p> 180041107</p>
+      </div>
+
+      <div class="box">
+        <div class="image">
+          <img src="image/Faysal.jpg" alt="">
+        </div>
+        <div class="name_job">Faysal Mahmud</div>
+        <div class="name_job"> Puloc</div>
+        <p> 180041117</p>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+  <script src="src/driver_rider.js"></script>
+      </body>
+</html>
